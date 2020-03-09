52eed1ac5ccf279b4f708efcb94fa78861c49008
salongley <salongley@icloud.com>
2020-03-09 13:58:53 +0000

    Added Local Storage

--- appleRings.js
+++ appleRings.js
@@ -27,23 +27,104 @@ var mainChart = new RadialProgressChart('.main', {
 
 function UpdateTarget3() {
     progress3 = progress3 + 1;
-    updateChart()
+    updateChart();
+    saveData();
 }
 
 function UpdateTarget2() {
     progress2 = progress2 + 1;
-    updateChart()
+    updateChart();
+    saveData();
+
 }
 function UpdateTarget1() {
     progress1 = progress1 + 1;
-    updateChart()
+    updateChart();
+    saveData();
+
 }
 
 function setUp() {
 
+    loadData();
+    updateChart();
+
     document.getElementById('Button3').innerHTML = GoalName3;
     document.getElementById('Button2').innerHTML = GoalName2;
     document.getElementById('Button1').innerHTML = GoalName1;
+}
+
+function loadData() {
+    if (localStorage.getItem("AppleRings.goal1") != undefined) {
+        goal1 = localStorage.getItem("AppleRings.goal1");
+    } else {
+        goal1 = 2;
+    };
+
+    if (localStorage.getItem("AppleRings.goal2") != undefined) {
+        goal2 = localStorage.getItem("AppleRings.goal2");
+    } else {
+        goal2 = 4;
+    };
+    if (localStorage.getItem("AppleRings.goal3") != undefined) {
+        goal3 = localStorage.getItem("AppleRings.goal3");
+    } else {
+        goal3 = 2;
+    };
+
+    if (localStorage.getItem("AppleRings.progress1") != undefined) {
+        progress1 = localStorage.getItem("AppleRings.progress1");
+    } else {
+        progress1 = 0;
+    };
+
+    if (localStorage.getItem("AppleRings.progress2") != undefined) {
+        progress2 = localStorage.getItem("AppleRings.progress2");
+    } else {
+        progress2 = 0;
+    };
+
+    if (localStorage.getItem("AppleRings.progress3") != undefined) {
+        progress3 = localStorage.getItem("AppleRings.progress3");
+    } else {
+        progress3 = 0;
+    };
+
+    if (localStorage.getItem("AppleRings.GoalName1") != undefined) {
+        GoalName1 = localStorage.getItem("AppleRings.GoalName1");
+    } else {
+        GoalName1 = "iUp";
+    };
+
+    if (localStorage.getItem("AppleRings.GoalName2") != undefined) {
+        GoalName2 = localStorage.getItem("AppleRings.GoalName2");
+    } else {
+        GoalName2 = "Business";
+    };
+    if (localStorage.getItem("AppleRings.GoalName3") != undefined) {
+        GoalName3 = localStorage.getItem("AppleRings.GoalName3");
+    } else {
+        GoalName3 = "Wow";
+    };
+
+
+
+
+}
+function saveData() {
+
+    localStorage.setItem("AppleRings.goal1", goal1);
+    localStorage.setItem("AppleRings.goal2", goal2);
+    localStorage.setItem("AppleRings.goal3", goal3);
+
+    localStorage.setItem("AppleRings.progress1", progress1);
+    localStorage.setItem("AppleRings.progress2", progress2);
+    localStorage.setItem("AppleRings.progress3", progress3);
+
+    localStorage.setItem("AppleRings.GoalName1", GoalName1);
+    localStorage.setItem("AppleRings.GoalName2", GoalName2);
+    localStorage.setItem("AppleRings.GoalName3", GoalName3);
+
 }
 
 function updateChart() {
@@ -61,9 +142,7 @@ function resetTargets() {
 
     document.getElementById('editTargetsForm').style.display = "none";
 
-    localStorage.progress1 = 0;
-    localStorage.progress2 = 0;
-    localStorage.progress3 = 0;
+    saveData();
 
 
 
@@ -105,18 +184,11 @@ function EditTargets() {
     document.getElementById('t1v').value = goal1;
     document.getElementById('t2v').value = goal2;
     document.getElementById('t3v').value = goal3;
+
+    saveData();
 }
 
 function Cancel() {
     document.getElementById('editTargetsForm').style.display = "none";
 
 }
-
-
-
-
-
-
-
-
-
